4a863bda29f3ad00df65d94e7c6c7130be61a993
gosoooo <kosh1013@gmail.com>
2021-11-04 00:05:39 +0900

    skill 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,20 @@
 <template>
     <header class="header">
-        <div class="header__inr" :style="{ backgroundImage:`url('${require('../assets/images/jeju.png')}')`}">
+        <div class="header__inr">
             <div class="header__cont">
-                <h1>
+                <h1 :style="{ backgroundImage:`url('${require('../assets/images/jeju.png')}')`}">
                     <span class="header__cont--text1">GO-SOOHYEON</span> 
                     UI developer
                 </h1>
-                <br />
-                <a href="https://gosoooo.notion.site/gosh-portfolio-373d0993b5cd4f349ef70d9f1d9fc24e" class="header__link--notion" ><span>경력 기술서</span></a>
+                <div>
+                    <a href="https://github.com/gosoohyeon/soooohome" class="header__link">Github</a>
+                    <a href="https://gosoooo.notion.site/gosh-portfolio-373d0993b5cd4f349ef70d9f1d9fc24e" class="header__link" ><span>경력 기술서</span></a>
+                </div>
+                <div class="header__intro">
+                    PC, mobile, hybrid web, Responsive web UI devolpment<br />
+                    SKILLS : HTML, SCSS, Gulp, Git, Zepline <br />
+                    STUDY : React.js, Vue.js
+                </div>
             </div>
         </div>
     </header>
